d6c9db5b035b7ec65459c83ef41276708b13d0a3
simal <simalchaudhari>
2025-01-04 14:23:30 +0530

    added changes

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,12 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
+  build: {
+    outDir: "dist", // Default output directory for Vite, ensure this matches your deployment setup
+  },
+  server: {
+    port: 3000, // Optional: Customize your dev server port
+  },
+  // Add base configuration for Vercel routing
+  base: "/",
 });
